2af3b4e7d4f238f6192517bb1da78ff8132afa14
Nardberjean <Nardberjean@users.noreply.github.com>
2018-04-15 08:18:13 -0400

    Create greeter.ts

---
+++ greeter.ts
@@ -0,0 +1,9 @@
+function greeter(person: string) {
+    return "Hello, " + person;
+}
+
+var user = "Jane User";
+
+document.body.innerHTML = greeter(user);
+
+// From: https://www.typescriptlang.org/docs/tutorial.html
