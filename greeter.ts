a6cd3e8f4a116cdc6c6b70f02b23039fdedf4dcf
Nardberjean <Nardberjean@users.noreply.github.com>
2018-05-01 11:57:01 -0400

    Update greeter.ts

--- greeter.ts
+++ greeter.ts
@@ -4,6 +4,7 @@ function greeter(person: string) {
 
 var user = "Jane User";
 
-document.body.innerHTML = greeter(user);
+// document.body.innerHTML = greeter(user);
+console.log(greeter(user));
 
 // From: https://www.typescriptlang.org/docs/tutorial.html
